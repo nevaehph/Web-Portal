2602eb53e4153a5b5a7a27e730dbfab5fb2333bd
Cyrmos <JunYan_67@hotmail.com>
2016-10-30 22:23:32 +0800

    Login
    Completed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,8 +7,22 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use GuzzleHttp\Client;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    private $client;
+
+    public function __construct() {
+       $this->client = new Client([
+       		'base_uri' => 'http://spapi.t05.sg/portal/'
+       ]);
+   	}
+
+    public function post($subURL, $array) {
+    	$response = $this->client->request('POST', $subURL, $array);
+    	return $response->getBody();
+    }
 }
